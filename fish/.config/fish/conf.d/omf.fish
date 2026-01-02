7153425240e10669a1705058f9bbe7677ecd362d
Simone Cittadini <simone.cittadini@moltiplygroup.com>
2026-01-02 06:15:47 +0100

    removed fish

--- fish/.config/fish/conf.d/omf.fish
+++
@@ -1,8 +0,0 @@
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-source $OMF_PATH/init.fish
-
-set -g theme_use_abbreviated_branch_name yes
-set -g theme_git_worktree_support yes
