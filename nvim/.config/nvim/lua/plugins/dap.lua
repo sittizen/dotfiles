bbf2174abeaf133db69dcfd2411c751a8c5c2fbd
Simone Cittadini <simone.cittadini@moltiplygroup.com>
2026-01-08 11:16:30 +0100

    neovim dap conf

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -2,17 +2,19 @@ return {
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"rcarriga/nvim-dap-ui",
 			"mfussenegger/nvim-dap-python",
+			"rcarriga/nvim-dap-ui",
 			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
 		},
 		config = function()
 			local dap = require("dap")
 			local dapui = require("dapui")
 			local dap_python = require("dap-python")
 
-			require("dapui").setup({})
+			require("dapui").setup()
+			require("dap-python").setup()
+
 			require("nvim-dap-virtual-text").setup({
 				commented = true, -- Show virtual text alongside comment
 			})
@@ -40,9 +42,12 @@ return {
 				numhl = "DiagnosticSignWarn",
 			})
 
-			-- Automatically open/close DAP UI
 			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
+			end
+
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
 			end
 
 			vim.keymap.set("n", "<leader>db", function()
@@ -65,11 +70,19 @@ return {
 				dap.step_out()
 			end, { desc = "Step Out" })
 
+			vim.keymap.set("n", "<leader>dk", function()
+				dap.step_out()
+			end, { desc = "Step Back" })
+
+			vim.keymap.set("n", "<leader>dr", function()
+				dap.run_to_cursor()
+			end, { desc = "Run to cursor" })
+
 			vim.keymap.set("n", "<leader>dq", function()
 				require("dap").terminate()
 			end, { desc = "Terminate debugging" })
 
-			vim.keymap.set("n", "<leader>du", function()
+			vim.keymap.set("n", "<leader>dt", function()
 				dapui.toggle()
 			end, { desc = "Toggle DAP UI" })
 		end,
