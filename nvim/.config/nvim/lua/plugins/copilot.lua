19e8a84954fd6b4cd548cd7113483c9eba113ca9
Simone Cittadini <simone.cittadini@moltiplygroup.com>
2026-01-13 15:27:00 +0100

    neovim reenabled copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,14 +1,14 @@
 return {
 	"github/copilot.vim",
-	enabled = false,
+	enabled = true,
 	config = function()
 		vim.b.copilot_enabled = true
 		vim.g.copilot_filetypes = {
 			["*"] = false,
-			-- ["python"] = true,
-			-- ["sh"] = true,
-			--["bash"] = true,
-			-- ["lua"] = true,
+			["python"] = true,
+			["sh"] = true,
+			["bash"] = true,
+			["lua"] = true,
 		}
 		vim.g.copilot_no_tab_map = true
 		vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
